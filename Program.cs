42ec60ab4fce6c9cdb67ccf633c557c169cb9725
mein9 <mateo0221@gmail.com>
2024-07-24 15:09:04 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System.ComponentModel;
+
+namespace AppWeek3Part3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // make new instance and print it 
+            Rational rational1 = new Rational(5);
+            WriteRational(rational1);
+
+            Rational rational2 = new Rational(20, 5);
+            Rational rational3 = new Rational(30, 5);
+            Rational rational4 = new Rational(50, 40);
+
+            // testing negate method
+            rational1.Negate();
+            Console.WriteLine("Testing negate");
+            WriteRational(rational1);
+
+            // testing invert method
+            rational1.Invert();
+            Console.WriteLine("Testing invert");
+            WriteRational(rational1);
+
+            // testing toDouble
+            Console.WriteLine("Testing toDouble");
+            Console.WriteLine(rational1.ToDouble());
+
+            // testing reduce
+            Console.WriteLine("Testing reduce staring as 20 / 5");
+            WriteRational(rational2.Reduce());
+
+            // testing add
+            Console.WriteLine("Testing Add");
+            Rational sum = Rational.Add(rational3, rational4);
+            WriteRational(sum);
+        }
+        public static void WriteRational(Rational rational)
+        {
+            Console.Write(rational.numerator);
+            Console.Write("/");
+            Console.WriteLine(rational.denominator);
+        }
+
+    }
+}
---
+++ Rational.cs
@@ -0,0 +1,79 @@
+﻿
+namespace AppWeek3Part3
+{
+    public class Rational
+    {
+        public int numerator;
+
+        public int denominator;
+
+        public Rational()
+        {
+            numerator = 0;
+            denominator = 1;
+        }
+        public Rational(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        public Rational(int numerator)
+        {
+            this.numerator = numerator;
+            denominator = 1;
+        }
+        public void Negate()
+        {
+            numerator = -numerator;
+        }
+        public void Invert()
+        {
+            if (numerator == 0)
+            {
+                throw new InvalidOperationException("Cannot invert a rational number with a numerator of zero.");
+            }
+
+            int temp = numerator;
+            numerator = denominator;
+            denominator = temp;
+        }
+        public double ToDouble()
+        {
+            return (double)numerator / denominator;
+        }
+        public Rational Reduce()
+        {
+            int gcd = GCD(numerator, denominator);
+            int reducedNumerator = numerator / gcd;
+            int reducedDenominator = denominator / gcd;
+
+            // Ensure the denominator is positive
+            if (reducedDenominator < 0)
+            {
+                reducedNumerator = -reducedNumerator;
+                reducedDenominator = -reducedDenominator;
+            }
+
+            return new Rational(reducedNumerator, reducedDenominator);
+        }
+        private int GCD(int n1, int n2)
+        {
+            if (n2 == 0)
+            {
+                return n1;
+            }
+            else
+            {
+                return GCD(n2, n1 % n2);
+            }
+        }
+        public static Rational Add(Rational r1, Rational r2)
+        {
+            int commonDenominator = r1.denominator * r2.denominator;
+            int numeratorSum = (r1.numerator * r2.denominator) + (r2.numerator * r1.denominator);
+
+            Rational result = new Rational(numeratorSum, commonDenominator);
+            return result.Reduce();
+        }
+    }
+}
